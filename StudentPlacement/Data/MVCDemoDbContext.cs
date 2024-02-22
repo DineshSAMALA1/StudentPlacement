9d6696aa874d7f5fd02551dd6373dcfec0ceda1f
labuser <labuser@ORACLE>
2024-02-22 07:33:09 +0000

    Add project files.

---
+++ StudentPlacement/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPlacement.Models.Domain;
+
+namespace StudentPlacement.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> students{ get; set; }
+    }
+}
---
+++ StudentPlacement/Models/Domain/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentPlacement.Models.Domain
+{
+    public class Student
+    {
+
+        public Guid Id { get; set; }   
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }  
+       
+        public int Percentage {  get; set; }
+    }
+}
